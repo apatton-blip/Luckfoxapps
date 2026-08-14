5aee02d8e9ad0ebcdb11eb64cfa392e52a850aa8
Andrew Patton <apatton@tactacam.com>
2026-08-15 01:04:07 +0800

    Updates yolo-lib sample application.

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -2,6 +2,6 @@
 
 source /home/development/scripts/common.sh
 
-EXEC_PATH=${APP_BUILD_DIR}/${APP}
+OUT_DIR=${APP_DIR}/out
 
-scp ${EXEC_PATH} root@${DEFAULT_IP}:${LUCKFOX_HOME_DIR}/${APP}
+scp -r ${OUT_DIR} root@${DEFAULT_IP}:${LUCKFOX_HOME_DIR}/${APP}
